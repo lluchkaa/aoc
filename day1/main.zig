38ae51336bb397ed76416a7ad9ac356b2e4c3715
Yura Luchka <lluchkaa@gmail.com>
2025-12-06 12:30:00 +0200

    add day1 task2

--- day1/main.zig
+++ day1/main.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 
-fn parseToken(word: []const u8) !struct { i8, i32 } {
+fn parseToken(word: []const u8) !i32 {
     const dir: i8 = if (word[0] == 'L') -1 else 1;
     const step = try std.fmt.parseInt(i32, word[1..], 10);
 
-    return .{ dir, step };
+    return dir * step;
 }
 
-fn task1() !void {
+fn task() !void {
     var file = try std.fs.cwd().openFile("input.txt", .{});
     defer file.close();
 
@@ -20,21 +20,36 @@ fn task1() !void {
 
     var tokenizer = std.mem.tokenizeScalar(u8, buffer, '\n');
 
-    var start: i32 = 50;
-    var password: i32 = 0;
+    var dial: i32 = 50;
+    var password: u32 = 0;
+    var password2: u32 = 0;
+
+    const limit = 100;
 
     while (tokenizer.next()) |token| {
         const parsed = try parseToken(token);
-        start += parsed.@"0" * parsed.@"1";
-        start = @mod(start, 100);
-        // std.debug.print("{}, {}", .{start, password});
-        if (start == 0) {
+        password2 += @abs(@divTrunc(parsed, limit));
+
+        const sign: i32 = if (parsed < 0) -1 else 1;
+        const modulus: i32 = @intCast(@mod(@abs(parsed), limit));
+        const step: i32 = sign * modulus;
+
+        const oldDial = dial;
+        dial = dial + step;
+
+        if ((oldDial != 0 and dial <= 0) or dial >= limit) {
+            password2 += 1;
+        }
+
+        dial = @mod(dial, limit);
+
+        if (dial == 0) {
             password += 1;
         }
     }
-    std.debug.print("{}", .{password});
+    std.debug.print("{}, {}", .{ password, password2 });
 }
 
 pub fn main() !void {
-    try task1();
+    try task();
 }
